ccb5eff7a999b51c16a90ed5573df6a96f2c6fae
ndelvalle <nicolas.delvalle@gmail.com>
2016-05-14 16:10:29 +0200

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,39 @@
+function GeoJson(coordinates) {
+  return {
+    type: 'FeatureCollection',
+    features: [{
+      type: 'Feature',
+      geometry: {
+        type: 'MultiPoint',
+        coordinates: coordinates
+      }
+    }]
+  };
+}
+
+
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'));
+
+  var latlngList = [];
+  $.get('http://localhost:9090/missing/stats', function(response) {
+
+    var geoPoints = response.distinctLocs.map(function(geo) {
+      var loc = geo.loc;
+      latlngList.push(new google.maps.LatLng(loc[1], loc[0]));
+      return loc;
+    });
+
+    map.data.addGeoJson(new GeoJson(geoPoints));
+
+    var bounds = new google.maps.LatLngBounds();
+
+    latlngList.forEach(function(n){
+      bounds.extend(n);
+    });
+
+    map.setCenter(bounds.getCenter());
+    map.fitBounds(bounds);
+  });
+}
